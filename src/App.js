38298a52314611b1a90ad833639a418e98bf13c6
DerrickDarku <derrickalberto@ymail.com>
2020-07-13 09:57:28 +0000

    created store with redux

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Contactform from './components/Contactform';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+        <div className="App">
+     <div className="Contactform">
+       <h1><ol>Family</ol>
+       <ol>FRIENDS</ol>
+       <ol>Customers</ol>
+       </h1>
+     </div>
+      
+    </div>
+    <div className="col-md-6">
+      <h1>CONTACT</h1>
+    </div>
+        </div>
+      </div>
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Contactform.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Contactform() {
+    return (
+        <h1>FAMILY AND CUSTOMERS</h1>
+        
+    )
+}
+
+export default Contactform
